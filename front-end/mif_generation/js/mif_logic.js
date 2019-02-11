f67adb85b7e817eef1ed3ea1c831dcd67581d867
leafeonia <maoym@126.com>
2019-02-11 19:23:28 +0800

    Site updated: 2019-02-11 19:23:22

---
+++ front-end/mif_generation/js/mif_logic.js
@@ -0,0 +1,46 @@
+
+
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+
+
+
+$('#submit').click(function () {
+    var text = ($("#pic").val());
+    if(!text){
+        alert("请输入字符画");
+        return;
+    }
+    var screen_width = ($("#width").val());
+    var screen_height = ($("#height").val());
+    if(!screen_width || !screen_height){
+        alert("请输入屏幕尺寸");
+        return;
+    }
+    if(isNaN(screen_width) || isNaN(screen_height)){
+        alert("请输入合法的屏幕尺寸");
+        return;
+    }
+    var linetext = text.split("\n");
+    var height = linetext.length;
+    var width = 0;
+    for (var i=0;i<height;i++){ width="Math.max(width,linetext[i].length);" }="" if(width=""> screen_width || height > screen_height){
+        alert("字符画尺寸大于屏幕尺寸，转换失败");
+        return;
+    }
+    var up_blank = Math.floor((screen_height-height)/2);
+    var left_blank = Math.floor((screen_width-width)/2);
+    var output = "WIDTH=8;\nDEPTH=" +  screen_width*screen_height +";\n\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\n\nCONTENT BEGIN\n";
+    for (i=0;i < up_blank*screen_width;i++){
+        output += i.toString(16)+": 00;\n";
+    }
+    for (i=0;i</height;i++){>
